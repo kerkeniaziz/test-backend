4c7ed1807d6703f6f19e8d4df57df410e23b6b6f
kerkeniaziz <kerkeniaziz@gmail.com>
2025-05-14 15:36:21 +0100

    optimize code

--- src/note/note.module.ts
+++ src/note/note.module.ts
@@ -3,14 +3,11 @@ import { NoteService } from './note.service';
 import { NoteController } from './note.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Note } from './entities/note.entity';
-
 @Module({
-  imports: [
-        TypeOrmModule.forFeature([Note]),
-        
-      ],  
+  imports: [TypeOrmModule.forFeature([Note])],
   controllers: [NoteController],
   providers: [NoteService],
   exports: [TypeOrmModule],
 })
-export class NoteModule {}
+export class NoteModule {
+}
